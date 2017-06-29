0373b7a7c5b1b0f9b01b057d9372c4d815cbc038
bo-bok <bo-bok@users.noreply.github.com>
2017-06-29 15:35:06 +0100

    add static html for front page to index.php
    relates #5

--- theme/validity-theme/index.php
+++ theme/validity-theme/index.php
@@ -14,43 +14,170 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Validity</title>
+		<meta name="viewport" content="width=device-width initial-scale=1.0">
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<script>
+			var settings = {
+				animate 	: true,
+				showHint	: true
+			};
+		</script>
+		<script data-main="assets/js/app" src="assets/js/require.js"></script>
+	</head>
 
-		<?php
-		if ( have_posts() ) :
+	<body class="page-home">
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+		<div class="wrapper">
 
-			<?php
-			endif;
+			<div class="bg"></div>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<header>
+				<div class="outer">
+					<div class="inner">
+						<a href="index.html" class="home-link">Home</a>
+						<a href="#" class="toggle-navigation">Navigation</a>
+						<a href="https://cafdonate.cafonline.org/480"class="button button__fixed">Support Us</a>
+					</div>
+				</div>
+			</header>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+			<nav>
+				<div class="outer">
+					<div class="inner">
+						<ul class="primary-navigation">
+							<li class="primary-navigation__item"><a href="take-action.html">Take action</a></li>
+							<li class="primary-navigation__item"><a href="who-we-are.html">Who we are</a></li>
+							<li class="primary-navigation__item"><a href="my-home-my-choice.html">My home, my choice</a></li>
+							<li class="primary-navigation__item"><a href="schools-for-all.html">Schools for all</a></li>
+							<li class="primary-navigation__item"><a href="im-a-person.html">I'm a person</a></li>
 
-			endwhile;
+						</ul>
+					</div>
+				</div>
+			</nav>
 
-			the_posts_navigation();
+			<div class="splash-screen splash-screen--step-1">
+				<h1>Mental Disability Advocacy Centre now becomes&hellip;</h1>
+				<div>
+					<div class="logo"></div>
+				</div>
+			</div>
 
-		else :
+			<section class="section-1">
+				<div class="carousel inner transition">
+				    <ul class="carousel__items">
+				        <li>
+				        	<div>
+					        	<h1>Making Voices Valid</h1>
+					        	<p>We use the law to advance the rights of people with mental health issues &amp; intellectual disabilities worldwide</p>
+				        	</div>
+				        </li>
+				        <li style="background-image: url(assets/img/category/person.jpg);">
+				        	<div>
+					        	<h1>I'm a person</h1>
+					        	<p>Calling for an end to brutal systems of guardianship</p>
+				        	</div>
+				        </li>
+				        <li style="background-image: url(assets/img/category/schools.jpg);">
+				        	<div>
+					        	<h1>Schools for All</h1>
+					        	<p>Making sure no child is denied inclusive education</p>
+				        	</div>
+				        </li>
+				        <li style="background-image: url(assets/img/category/my_home.jpg);">
+				        	<div>
+					        	<h1>My Home, My Choice</h1>
+					        	<p>Reducing abuse in institutions and increasing community living</p>
+				        	</div>
+				        </li>
+				    </ul>
+				    <ul class="carousel__navigation">
+				    	<li data-position="2"><a href="im-a-person.html" class="button">I'm a person</a></li>
+				    	<li data-position="3"><a href="schools-for-all.html" class="button">Schools for all</a></li>
+				    	<li data-position="4"><a href="my-home-my-choice.html" class="button">My home, my choice</a></li>
+				    </ul>
+				    <p class="footnote">* Formerly Mental Disability Advocacy Centre</p>
+				</div>
+			</section>
 
-			get_template_part( 'template-parts/content', 'none' );
+			<section class="section-2 section-news">
+				<div class="outer full-height">
+					<div class="inner transition">
 
-		endif; ?>
+						<div class="news-articles">
+							<div class="article">
+								<a href="articles/russia-landmark-judgement.html" class="article__image" data-layout="3x4" style="background-image: url(assets/img/article-preview/Kotcherovs.jpg);"></a>
+								<h1 class="article__category">
+									<a href="im-a-person.html">I'm a person</a>
+								</h1>
+								<p class="article__title">
+									<a href="articles/russia-landmark-judgement.html">Russia: Landmark judgment on parenting rights for persons with disabilities
+</a>
+								</p>
+							</div>
+							<div class="article">
+								<a href="articles/legal-capacity-kenya.html" class="article__image" data-layout="3x3.7" style="background-image: url(assets/img/article-preview/kenya.jpg);"></a>
+								<h1 class="article__category">
+									<a href="im-a-person.html">I'm a person</a>
+								</h1>
+								<p class="article__title">
+									<a href="articles/legal-capacity-kenya.html">Legal capacity in Kenya</a>
+								</p>
+							</div>
+							<div class="article">
+								<a href="articles/slovakia-denial-education.html" class="article__image" data-layout="3x2.5" style="background-image: url(assets/img/article-preview/SLOVAKIA-SUPREME-COURT.jpg);"></a>
+								<h1 class="article__category">
+									<a href="schools-for-all.html">Schools for all</a>
+								</h1>
+								<p class="article__title">
+									<a href="articles/slovakia-denial-education.html">Slovakia: supreme court rules that denial of inclusive education to children with disabilities can amount to discrimination
+</a>
+								</p>
+							</div>
+							<div class="article">
+								<a href="articles/rusi-stanev.html" class="article__image" data-layout="3x4" style="background-image: url(assets/img/article-preview/RUSI-STANEV.jpg);"></a>
+								<h1 class="article__category">
+									<a href="my-home-my-choice.html">My home, my choice</a>
+								</h1>
+								<p class="article__title">
+									<a href="articles/rusi-stanev.html">A tribute to Rusi Stanev</a>
+								</p>
+							</div>
+						</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</div>
+				</div>
+			</section>
 
-<?php
-get_sidebar();
-get_footer();
+			<section class="section-3">
+				<div class="outer full-height centered with-footer">
+					<div class="inner transition has-button">
+						<div class="cta">
+							<p>We challenge laws to give people the rights they are entitled to. We fight to make their voices valid. With firm roots in local communities, we are changing the way entire nations treat mental disability. We fearlessly represent people through the courts.</p>
+							<a href="https://cafdonate.cafonline.org/480"class="button">Donate</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<footer>
+				<div class="outer">
+					<div class="inner">
+						<ul class="social">
+							<li class="social__item"><a href="https://www.facebook.com/mentaldisabilityadvocacy/">Facebook</a></li>
+							<li class="social__item"><a href="https://twitter.com/MDACintl">Twitter</a></li>
+						</ul>
+						<p>UK Registered charity number 1104456</p>
+					</div>
+				</div>
+			</footer>
+
+		</div>
+
+	</body>
+</html>
