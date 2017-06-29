76c1bd448ed901e55ae1bc1115bd78c04479dbcb
bo-bok <bo-bok@users.noreply.github.com>
2017-06-29 23:48:53 +0100

    separates underscores boilerplates and template hierarchy
    relates #5

--- theme/validity-theme/functions.php
+++ theme/validity-theme/functions.php
@@ -90,28 +90,28 @@ add_action( 'after_setup_theme', 'validity_setup' );
  *
  * @global int $content_width
  */
-function validity_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'validity_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'validity_content_width', 0 );
+// function validity_content_width() {
+// 	$GLOBALS['content_width'] = apply_filters( 'validity_content_width', 640 );
+// }
+// add_action( 'after_setup_theme', 'validity_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function validity_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'validity' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'validity' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'validity_widgets_init' );
+// function validity_widgets_init() {
+// 	register_sidebar( array(
+// 		'name'          => esc_html__( 'Sidebar', 'validity' ),
+// 		'id'            => 'sidebar-1',
+// 		'description'   => esc_html__( 'Add widgets here.', 'validity' ),
+// 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 		'after_widget'  => '</section>',
+// 		'before_title'  => '<h2 class="widget-title">',
+// 		'after_title'   => '</h2>',
+// 	) );
+// }
+// add_action( 'widgets_init', 'validity_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
