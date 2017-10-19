4810ff5948c9a5033c30f5d85ed18c1a47d94013
bo-bok <jessicasalmon05@gmail.com>
2017-10-19 15:24:53 +0100

    create new tempalte for individual staff page with basic layout relates #83

---
+++ theme/validity-theme/page-staff-profile.php
@@ -0,0 +1,41 @@
+<?php
+/*
+  Template Name: Staff Profile Page
+*/
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package validity
+ */
+
+get_header(); ?>
+<body class="page-staff-profile">
+  <!-- body class will have some margin -->
+  <div class="wrapper">
+    <?php get_sidebar(); ?> <!-- sidebar = nav -->
+
+    <section>
+      <p>Jessica Salmon</p>
+      <div>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+      </div>
+
+      <div>Location and contact details</div>
+      <div>Follow him/her on Twitter [optional] @jessicasalmon</div>
+
+      <button>Back to staff page</button>
+
+    </section>
+    </div>
+</body>
+
+<?php get_footer(); ?>
