77de1212061204541b473f0aa3a39070a3bcac54
bo-bok <jessicasalmon05@gmail.com>
2017-10-23 15:43:29 +0100

    create and style contact page with hard coded data add note to readme relates #79

---
+++ theme/validity-theme/page-contact.php
@@ -0,0 +1,62 @@
+<?php
+/*
+  Template Name: Contact Page
+*/
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package validity
+ */
+
+get_header(); ?>
+
+<body class="page-contact">
+  <div class="wrapper">
+  <?php get_sidebar(); ?>
+
+    <section class="section-1">
+      <div class="outer full-height centered with-footer">
+        <div class="inner transition">
+
+
+      <div class="section-contact">
+        <p class="section-contact-intro">We are based in London and Budapest <br />
+        Feel free to contact us:</p>
+          <main class="section-contact-address">
+            <div class="address">
+              <p class="address-info">Regus EMKE</p>
+              <p class="address-info">Rákóczi út 42.</p>
+              <p class="address-info">Budapest</p>
+              <p class="address-info">1072 Hungary</p>
+              <p class="address-info">tel: +361 327 4247</p>
+              <p class="address-info">fax: +361 267 9100</p>
+              <p class="address-info">email: mdac@mdac.org</p>
+            </div>
+            <div class="address">
+              <p class="address-info">Regus EMKE</p>
+              <p class="address-info">Rákóczi út 42.</p>
+              <p class="address-info">Budapest</p>
+              <p class="address-info">1072 Hungary</p>
+              <p class="address-info">tel: +361 327 4247</p>
+              <p class="address-info">fax: +361 267 9100</p>
+              <p class="address-info">email: mdac@mdac.org</p>
+            </div>
+          </main>
+      </div>
+
+
+        </div>
+      </div>
+    </section>
+
+  </div>
+</body>
+<?php
+get_footer(); ?>
