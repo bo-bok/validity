d3059c0d9f114b664dd8a016c1a5380226648944
bo-bok <jessicasalmon05@gmail.com>
2017-11-09 18:30:41 +0000

    footer text and social media urls are editable, 2 extra fields added (styling to be added) relates #94

--- theme/validity-theme/footer.php
+++ theme/validity-theme/footer.php
@@ -15,11 +15,21 @@
   <footer>
     <div class="outer">
       <div class="inner">
+      <?php $footerContent = new WP_Query(array(
+          'post_type' => 'footer'
+        )); ?>
+
+        <?php while ($footerContent->have_posts()) : $footerContent->the_post(); ?>
         <ul class="social">
-          <li class="social__item"><a href="https://www.facebook.com/mentaldisabilityadvocacy/">Facebook</a></li>
-          <li class="social__item"><a href="https://twitter.com/ValidityNGO">Twitter</a></li>
+          <li class="social__item"><a href="https://www.facebook.com/<?php the_field('facebook_url') ?>">Facebook</a></li>
+          <li class="social__item"><a href="https://twitter.com/<?php the_field('twitter_url') ?>">Twitter</a></li>
         </ul>
-        <p>UK Registered charity number 1104456</p>
+
+        <p><?php the_field('info_line_1') ?></p>
+        <p><?php the_field('info_line_2') ?></p>
+        <p><?php the_field('info_line_3') ?></p>
+        <?php endwhile; ?>
+
       </div>
     </div>
   </footer>
