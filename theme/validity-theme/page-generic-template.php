9bb573728873cf676a018b41a9af314ac6ce4c1a
bo-bok <jessicasalmon05@gmail.com>
2017-11-09 14:36:30 +0000

    generic template for text with no image relates #80

--- theme/validity-theme/page-generic-template.php
+++ theme/validity-theme/page-generic-template.php
@@ -18,31 +18,30 @@
 get_header(); ?>
 
 
-<body class="page-staff-profile">
+<body class="page-generic-template">
   <div class="wrapper">
   <?php get_sidebar(); ?> <!-- sidebar = nav -->
 
   <section class="section-1">
-    <div class="outer full-height centered" id="honorary-president">
+    <div class="outer full-height centered">
       <div class="inner transition">
 
-        <div class="who-we-are-members">
-          <div class="group">
-            <div class="overview">
-              <h1 class="group-heading">
-                <p><?php the_title(); ?></p>
-              </h1>
+        <div class="generic-template-content">
+          <div class="generic-template-content-container">
+            <h1 class="group-heading">
+              <p><?php the_title(); ?></p>
+            </h1>
 
-              <?php
-                // TO SHOW THE PAGE CONTENTS
-                while ( have_posts() ) : the_post(); ?>
+            <?php
+              // TO SHOW THE PAGE CONTENTS
+              while ( have_posts() ) : the_post(); ?>
               <p><?php the_content(); ?></p>
-              <?php
-                endwhile; //resetting the page loop
-                // wp_reset_query(); //resetting the page query
-                ?>
-              <a href="/who-we-are" onclick="window.history.go(-1)"  class="button_transparent anchor">Back</a>
-            </div>
+            <?php
+              endwhile; //resetting the page loop
+              // wp_reset_query(); //resetting the page query
+              ?>
+              <!-- WHAT A HREF TO PUT HERE -->
+            <a href="/who-we-are" onclick="window.history.go(-1)"  class="button_transparent anchor">Back</a>
           </div>
         </div>
       </div>
