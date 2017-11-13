7877702f24c4c90543ee3446f46e15dcc9d005b6
bo-bok <jessicasalmon05@gmail.com>
2017-11-13 14:27:43 +0000

    footer is now dynamic styling WIP relates #94

---
+++ theme/validity-theme/footer-old.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package validity
+ */
+
+?>
+<!-- </div> -->
+<!-- #content -->
+  <footer>
+    <div class="outer">
+      <div class="inner">
+      <?php $footerContent = new WP_Query(array(
+          'post_type' => 'footer'
+        )); ?>
+
+        <?php while ($footerContent->have_posts()) : $footerContent->the_post(); ?>
+        <ul class="social">
+          <li class="social__item"><a href="https://www.facebook.com/<?php the_field('facebook_url') ?>">Facebook</a></li>
+          <li class="social__item"><a href="https://twitter.com/<?php the_field('twitter_url') ?>">Twitter</a></li>
+        </ul>
+        
+
+        <p><?php the_field('info_line_1') ?></p>
+        <p><?php the_field('info_line_2') ?></p>
+        <p><?php the_field('info_line_3') ?></p>
+        <?php endwhile; ?>
+
+      </div>
+    </div>
+  </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- theme/validity-theme/footer.php
+++ theme/validity-theme/footer.php
@@ -19,17 +19,24 @@
           'post_type' => 'footer'
         )); ?>
 
-        <?php while ($footerContent->have_posts()) : $footerContent->the_post(); ?>
-        <ul class="social">
-          <li class="social__item"><a href="https://www.facebook.com/<?php the_field('facebook_url') ?>">Facebook</a></li>
-          <li class="social__item"><a href="https://twitter.com/<?php the_field('twitter_url') ?>">Twitter</a></li>
-        </ul>
+        <div class="footer__container">
+          <img src="assets/img/logo-footer.png" style="width: 20px; height: 20px"/>
+          <?php while ($footerContent->have_posts()) : $footerContent->the_post(); ?>
 
-        <p><?php the_field('info_line_1') ?></p>
-        <p><?php the_field('info_line_2') ?></p>
-        <p><?php the_field('info_line_3') ?></p>
-        <?php endwhile; ?>
+          <ul>
+            <li class=""><a href="https://www.facebook.com/<?php the_field('facebook_url') ?>">Facebook</a></li>
+            <li class=""><a href="https://twitter.com/<?php the_field('twitter_url') ?>">Twitter</a></li>
+          </ul>
 
+          <div>
+            <p><?php the_field('info_line_1') ?></p>
+            <p><?php the_field('info_line_2') ?></p>
+            <p><?php the_field('info_line_3') ?></p>
+          </div>
+
+
+          <?php endwhile; ?>
+        </div>
       </div>
     </div>
   </footer>
